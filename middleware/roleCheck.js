c6628b45d2c5cd0c37ed8bcbbe75c45c7214823b
duyhieufpt123 <dumaconcacditmemay@gmail.com>
2024-03-14 22:41:52 +0700

    update authenticate

--- middleware/roleCheck.js
+++ middleware/roleCheck.js
@@ -1,14 +1,14 @@
 const Role = require('../models/Role');
 
-const roleCheck = (roleName) => {
+const roleCheck = (allowedRoles) => {
   return async (req, res, next) => {
     try {
-      const role = await Role.findOne({ name: roleName });
+      const role = await Role.findById(req.account.roleId);
       if (!role) {
-        return res.status(400).send({ error: `Role ${roleName} does not exist.` });
+        return res.status(400).send({ error: 'Role does not exist.' });
       }
 
-      if (!req.account.roleId.equals(role._id)) {
+      if (!allowedRoles.includes(role.name)) {
         return res.status(403).send({ error: 'Permission denied.' });
       }
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,12 +5,12 @@ const { auth } = require('../middleware/auth');
 const roleCheck = require('../middleware/roleCheck');
 
 
-router.post('/', auth, roleCheck('admin'), productController.createProduct);
-router.put('/:id', auth, roleCheck('admin'), productController.updateProduct);
+router.post('/', auth, roleCheck(['admin', 'staff']), productController.createProduct);
+router.put('/:id', auth, roleCheck(['admin', 'staff']), productController.updateProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.get('/:name', productController.getProductByName);
 
-router.delete('/:id', auth, roleCheck('admin'), productController.deleteProduct)
+router.delete('/:id', auth, roleCheck(['admin', 'staff']), productController.deleteProduct)
 
 module.exports = router;
