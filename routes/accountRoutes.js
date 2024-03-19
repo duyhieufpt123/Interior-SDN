aa341f7a7ab2a79fcfe97a8a02bb44b2ac63e925
Duy Hiếu <112913950+duyhieufpt123@users.noreply.github.com>
2024-03-19 15:36:46 +0700

    Update accountRoutes.js

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -14,6 +14,7 @@ router.post('/resetPassword/:token', accountController.resetPassword);
 // cái nào có auth nghĩa là cái đó cần token 
 router.get('/profile', auth, accountController.getProfile);
 router.put('/profile', auth, accountController.updateProfile);
+router.post('/register', auth, roleCheck('admin'), accountController.registerForStaff);
 
 //cái nào vừa có auth vừa có rolecheck admin thì kiếm token của admin thì mới làm việc dược
 router.delete('/:id', auth, roleCheck('admin'), accountController.deleteAccount);
